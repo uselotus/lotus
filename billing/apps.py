072bf153566d427cb8cb60eea8a02f4860047479
Mason Acevedo <macevedo@hmc.edu>
2022-07-21 16:46:43 -0400

    Test Update
    This is Mason just making sure github is working as it's supposed to. All I've done is add two comments to the lotus\billing\apps.py file

--- billing/apps.py
+++ billing/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
+# Mason has added a comment in the apps file 
+# (just making sure github is working)
 class BillingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'billing'
