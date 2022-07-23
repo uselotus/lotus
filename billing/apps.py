a1c91ab60d0835f02879e66f9ecdffee3b116e58
mnida <mikaelnida@gmail.com>
2022-07-22 22:41:06 -0700

    Added Pre-Commit + Pipenv In Workflow

--- billing/apps.py
+++ billing/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BillingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'billing'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "billing"
