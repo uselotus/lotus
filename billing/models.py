404e633e1b9c86b24d7905da5d5e374961394d18
Mason Acevedo <macevedo@hmc.edu>
2022-07-23 16:08:45 -0400

    Simplified Customer Model
    Removed the vast majority of fields

--- billing/models.py
+++ billing/models.py
@@ -3,6 +3,7 @@ from django.db import models
 import uuid
 from model_utils import Choices
 from django.utils.translation import gettext_lazy as _
+from djmoney.models.fields import MoneyField
 
 
 # Create your models here.
@@ -13,64 +14,17 @@ class Customer(models.Model):
     Customer object. An explanation of the Customer's fields follows:
     first_name: self-explanatory
     last_name: self-explanatory
-    billing_id: internal billing system identifier
-    system_id: customer's id within the users backend system
-    billing_address: currently set to null, but we will need to set this to an "address" object later
+    company_name: self-explanatory
     email_address: self-explanatory
     phone_number: self-explanatory
-    balance: An amount of money that the customer owes the company.
-             If the number is positive, the customer owes money to the company,
-             if negative, then the company owes money to the customer.
-    time_created: The time at which the customer object was created
-    default_payment_source: The customer's payment method. Currently set to a string,
-                            but will probably need to be a full-on 'payment' object in the future.
-    discount: The discount that applies to this customer, if any. Will need a "discount" object in the future.
-    invoice_prefix: A prefix string to put on the customer's invoice, so that we may generate unique invoice numbers
-    invoice_settings: The customer's default invoice settings
-    next_invoice_sequence: Suffix of the customer's next invoice number (i.e. counting number for invoices)
-    preferred_locales: Customer's preferred languages, ordered by preference
-    subscriptions: Customer's current subscriptions. Need to make into a list or array of subscription objects
-    tax: Tax details for the customer. Will need to expand on later.
-    tax_exempt: String that takes 1 of 3 values:
-                1. none
-                2. exempt
-                3. reverse
-
-    tax_ids: Customer's tax IDs.
-    test_clock: ID of test_clock this customer belongs to
-
     """
 
     first_name = models.CharField(max_length=30)  # 30 characters is arbitrary
     last_name = models.CharField(max_length=30)
-    billing_id = models.CharField(
-        max_length=100, blank=True, unique=True, default=uuid.uuid4
-    )
-    system_id = models.CharField(max_length=100, unique=True)
-    # billing_address = null
-    email_address = models.CharField(max_length=30)  # 30 chars is arbitrary
-    phone_number = models.CharField(max_length=30)
-    # balance = MoneyField(
-    #     decimal_places=2,
-    #     default=0,
-    #     default_currency="USD",
-    #     max_digits=11,
-    # )
+    company_name = models.CharField(max_length=30)
 
-    time_created: models.DateField = models.DateTimeField(auto_now_add=True)
-    default_payment_source = models.CharField(
-        max_length=60
-    )  # 60 characters is arbitrary,
-    # discount = null
-    invoice_prefix = models.CharField(max_length=30)  # 30 chars is arbitrary
-    # invoice_settings = null
-    next_invoice_sequence = models.CharField(max_length=30)  # 30 chars arbitrary
-    # preferred_locales = ArrayField(
-    #     models.CharField(max_length=30), blank=True, size=3
-    # )  # I think 3 is the number of languages, not sure
-    # subscriptions = ArrayField(null, size=10)
-    # tax = null
-    tax_exempt = models.CharField(max_length=30)
+    def __init__(self: _Self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 
 class Event(models.Model):
