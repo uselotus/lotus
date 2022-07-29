84fd87186e93b68ffa01bbde84167a7d41a0ebce
mnida <mikaelnida@gmail.com>
2022-07-29 01:05:54 -0700

    Added Celery Beat Worker For Generating Invoices

--- billing/tasks.py
+++ billing/tasks.py
@@ -2,27 +2,14 @@ from __future__ import absolute_import, unicode_literals
 from webbrowser import get
 from celery import shared_task
 from datetime import datetime
-from .views import UsageView
+from .models import Subscription
+from .generate_invoice import generate_invoice
 
 
-@shared_task(name="print_msg_main")
-def print_message(message, *args, **kwargs):
-    print(f"Celery is working!! Message is {message}")
-
-
-@shared_task(name="print_time")
-def print_time():
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    print(f"Current Time is {current_time}")
-
-
-@shared_task(name="get_calculation")
-def calculate(val1, val2):
-    total = val1 + val2
-    return total
-
-
-@shared_task(name="calculate_invoice")
-def calculate_invoice(customer_id):
-    UsageView.get()
+@shared_task
+def calculate_invoice(subscription):
+    ending_subscriptions = Subscription.objects.filter(
+        status="active", end_date__lte=datetime.now()
+    )
+    for subscription in ending_subscriptions:
+        generate_invoice(subscription)
--- lotus/urls.py
+++ lotus/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from billing.views import (
+from billing.views.views import (
     EventViewSet,
     SubscriptionViewSet,
     CustomerView,
