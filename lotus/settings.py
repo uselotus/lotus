de00f534143b47d06b384cffad13eea7b4837cc4
mnida <mikaelnida@gmail.com>
2022-07-29 10:42:53 -0700

    Initial doppler + docker_compose file

--- lotus/settings.py
+++ lotus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q&t0uoz(b(^la!u2bpj@t3l4jl(=v!7(etlp!t7wb(u)xk-#th"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
