b90706d2313cce523d52a6335773423f2530a68c
mnida <mikaelnida@gmail.com>
2022-08-03 13:27:08 -0700

    add tenant app

---
+++ tenant/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django_tenants.admin import TenantAdminMixin
+
+from tenant.models import Tenant, User, Domain, APIToken
+
+
+from rest_framework_api_key.admin import APIKeyModelAdmin
+
+
+@admin.register(APIToken)
+class UserAPIKeyModelAdmin(APIKeyModelAdmin):
+    pass
+
+
+@admin.register(Tenant)
+class TenantAdmin(TenantAdminMixin, admin.ModelAdmin):
+    list_display = ("company_name", "id")
+
+
+admin.site.register(
+    User,
+)
