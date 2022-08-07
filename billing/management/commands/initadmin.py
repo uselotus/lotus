4ee34e91d4d9f7c711535514948ada7d4289b046
mnida <mikaelnida@gmail.com>
2022-08-07 11:27:41 -0700

    Fix celery and env for local

--- billing/management/commands/initadmin.py
+++ billing/management/commands/initadmin.py
@@ -18,15 +18,31 @@ class Command(BaseCommand):
                 company_name="Lotus Public",
             )
             tenant.save()
+            domain = Domain()
+            domain.domain = "www.uselotus.app"
+            domain.tenant = tenant
+            domain.is_primary = True
+            domain.save()
 
         else:
             tenant = Tenant.objects.filter(schema_name="public").first()
         # Add one or more domains for the tenant
-        domain = Domain()
-        domain.domain = "www.uselotus.app"
-        domain.tenant = tenant
-        domain.is_primary = True
-        domain.save()
+
+        # from django_celery_beat.models import CrontabSchedule, PeriodicTask
+
+        # schedule, _ = CrontabSchedule.objects.get_or_create(
+        #     minute="1",
+        #     hour="*",
+        #     day_of_week="*",
+        #     day_of_month="*",
+        #     month_of_year="*",
+        # )
+
+        # PeriodicTask.objects.create(
+        #     crontab=schedule,
+        #     name="Generate Invoices",
+        #     task="billing.tasks.calculate_invoice",
+        # )
 
         username = os.getenv("DJANGO_SUPERUSER_USERNAME")
         email = os.getenv("DJANGO_SUPERUSER_EMAIL")
