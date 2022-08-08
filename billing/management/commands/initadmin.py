ed2cd5bebeb0133e3ef548ee3573502da2e83277
mnida <mikaelnida@gmail.com>
2022-08-07 21:51:22 -0700

    Fix celery-beat in docker and add initadmin tenant commands

--- billing/management/commands/initadmin.py
+++ billing/management/commands/initadmin.py
@@ -2,30 +2,49 @@ from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
 from tenant.models import User, Tenant, Domain, APIToken
+from django.core.management import call_command
 from dotenv import load_dotenv
+from django.db import connection
 import os
 
 load_dotenv()
+
+
+def create_local_tenant():
+    tenant = Tenant.objects.create(
+        company_name="Local",
+        schema_name="local",
+    )
+    tenant.save()
+    domain = Domain.objects.create(
+        tenant=tenant,
+        domain="local.localhost",
+        is_primary=True,
+    )
+    domain.save()
+    return tenant
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
         if not Tenant.objects.filter(schema_name="public"):
-            print("30")
             tenant = Tenant.objects.create(
                 schema_name="public",
                 company_name="Lotus Public",
             )
             tenant.save()
             domain = Domain()
-            domain.domain = "www.uselotus.app"
+            domain.domain = "localhost"
             domain.tenant = tenant
             domain.is_primary = True
             domain.save()
 
         else:
             tenant = Tenant.objects.filter(schema_name="public").first()
+            print("Public Tenant found")
+
+        local_tenant = create_local_tenant()
         # Add one or more domains for the tenant
 
         # from django_celery_beat.models import CrontabSchedule, PeriodicTask
@@ -49,9 +68,15 @@ class Command(BaseCommand):
         password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
         if not User.objects.filter(username=username).exists():
+            connection.set_tenant(local_tenant)
             print("Creating account for %s (%s)" % (username, email))
             admin = User.objects.create_superuser(
                 email=email, username=username, password=password
             )
+            api_token = APIToken.objects.create(
+                user=admin,
+                name="local_token",
+            )
+            api_token.save()
         else:
             print("Admin account has already been initialized.")
