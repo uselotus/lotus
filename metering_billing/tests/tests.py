dc6af6e5c2120d5b60d89e0f9cba2e184d22fcfc
Diego Escobedo <diego@escobedo.com>
2022-08-17 02:37:46 -0500

    small changes for readability

--- metering_billing/tests/tests.py
+++ metering_billing/tests/tests.py
@@ -87,7 +87,7 @@ class SubscriptionTest(TestCase):
             "customer_id": "7fa09280-957c-3w5f-925a-6a3498a1d299",
             "organization_id": self.organization_id,
         }
-        self.client.force_authenticate(user=user_object)
+        # self.client.force_authenticate(user=user_object)
 
     def test_subscription_create_success(self):
         """
@@ -133,136 +133,135 @@ class SubscriptionTest(TestCase):
 
         response = self.client.get(
             reverse("usage"),
-            data=json.dumps(usage_payload, cls=DjangoJSONEncoder),
-            content_type="application/json",
+            data=usage_payload,
             **self.authorization_header,
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.body, 1)
 
-# class TrackEventTest(TestCase):
-#     """
-#     Testcases for the track_event function view.
-#     """
+class TrackEventTest(TestCase):
+    """
+    Testcases for the track_event function view.
+    """
 
-#     def setUp(self):
+    def setUp(self):
 
-#         user_object = User.objects.create_user(
-#             username="test", 
-#             email=""
-#         )
-#         organization_object = Organization.objects.create(
-#             company_name="Test Company",
-#         )
-#         self.organization_id = getattr(organization_object, 'id')
-#         organization_object.users.add(user_object)
-#         Customer.objects.create(
-#             customer_id="7fa09280-957c-4a5f-925a-6a3498a1d299",
-#             organization=organization_object,
-#         )
-#         api_key, key = APIToken.objects.create_key(
-#             name="test-api-key", 
-#             organization=organization_object
-#             )
+        user_object = User.objects.create_user(
+            username="test", 
+            email=""
+        )
+        organization_object = Organization.objects.create(
+            company_name="Test Company",
+        )
+        self.organization_id = getattr(organization_object, 'id')
+        organization_object.users.add(user_object)
+        Customer.objects.create(
+            customer_id="7fa09280-957c-4a5f-925a-6a3498a1d299",
+            organization=organization_object,
+        )
+        api_key, key = APIToken.objects.create_key(
+            name="test-api-key", 
+            organization=organization_object
+            )
 
-#         self.client = APIClient()
-#         # self.client.credentials(HTTP_AUTHORIZATION="Api-Key" + " " + key)
-#         self.authorization_header = {
-#             "Authorization": "Api-Key" + " " + key,
-#         }
+        self.client = APIClient()
+        # self.client.credentials(HTTP_AUTHORIZATION="Api-Key" + " " + key)
+        self.authorization_header = {
+            "Authorization": "Api-Key" + " " + key,
+        }
 
-#         idempotency_id = uuid.uuid4()
-#         self.valid_payload = {
-#             "event_name": "Emails",
-#             "properties": {"email": ""},
-#             "idempotency_id": idempotency_id,
-#             "time_created": "2022-07-25T01:11:42.535Z",
-#             "customer_id": "7fa09280-957c-4a5f-925a-6a3498a1d299",
-#             "organization_id": self.organization_id,
-#         }
-#         self.invalid_payload = {
-#             "event_name": "Emails",
-#             "properties": {"email": ""},
-#             "idempotency_id": idempotency_id,
-#             "time_created": "2022-07-25T01:11:42.535Z",
-#             "customer_id": "7fa09280-957c-3w5f-925a-6a3498a1d299",
-#             "organization_id": self.organization_id,
-#         }
+        idempotency_id = uuid.uuid4()
+        self.valid_payload = {
+            "event_name": "Emails",
+            "properties": {"email": ""},
+            "idempotency_id": idempotency_id,
+            "time_created": "2022-07-25T01:11:42.535Z",
+            "customer_id": "7fa09280-957c-4a5f-925a-6a3498a1d299",
+            "organization_id": self.organization_id,
+        }
+        self.invalid_payload = {
+            "event_name": "Emails",
+            "properties": {"email": ""},
+            "idempotency_id": idempotency_id,
+            "time_created": "2022-07-25T01:11:42.535Z",
+            "customer_id": "7fa09280-957c-3w5f-925a-6a3498a1d299",
+            "organization_id": self.organization_id,
+        }
 
-#     def test_track_event_if_customer_does_not_exist(self):
-#         """
-#         Test that track_event returns bad request with message "Customer does not exist" if the customer's customer_id does not exist in the database.
-#         """
-#         x = 1
-#         response = self.client.post(
-#             reverse("track_event"),
-#             data=json.dumps(self.invalid_payload, cls=DjangoJSONEncoder),
-#             content_type="application/json",
-#             **self.authorization_header,
-#         )
-#         self.assertEqual(response.content, b"Customer does not exist")
+    def test_track_event_if_customer_does_not_exist(self):
+        """
+        Test that track_event returns bad request with message "Customer does not exist" if the customer's customer_id does not exist in the database.
+        """
+        x = 1
+        response = self.client.post(
+            reverse("track_event"),
+            data=json.dumps(self.invalid_payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+            **self.authorization_header,
+        )
+        self.assertEqual(response.content, b"Customer does not exist")
 
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-#     def test_valid_track_event_creates_event(self):
-#         """
-#         Test that track_event creates an event in the database.
-#         """
-#         self.assertEqual(
-#             Event.objects.all()
-#             .filter(idempotency_id=self.valid_payload["idempotency_id"])
-#             .count(),
-#             0,
-#         )
-#         response = self.client.post(
-#             reverse("track_event"),
-#             data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
-#             content_type="application/json",
-#         )
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(
-#             Event.objects.all()
-#             .filter(idempotency_id=self.valid_payload["idempotency_id"])
-#             .count(),
-#             1,
-#         )
+    def test_valid_track_event_creates_event(self):
+        """
+        Test that track_event creates an event in the database.
+        """
+        self.assertEqual(
+            Event.objects.all()
+            .filter(idempotency_id=self.valid_payload["idempotency_id"])
+            .count(),
+            0,
+        )
+        response = self.client.post(
+            reverse("track_event"),
+            data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            Event.objects.all()
+            .filter(idempotency_id=self.valid_payload["idempotency_id"])
+            .count(),
+            1,
+        )
 
-#     def test_idempotency_duplicate_does_not_create_multiple_events(self):
-#         """
-#         Test that track_event does not create multiple events in the database if the idempotency_id is duplicated.
-#         """
-#         #first, ensure that the event_id we are sending is not cuirrently present
-#         self.assertEqual(
-#             Event.objects.all()
-#                         .filter(idempotency_id=self.valid_payload["idempotency_id"])
-#                         .count(),
-#             0
-#         )
-#         #then send event, and expect success
-#         response = self.client.post(
-#             reverse("track_event"),
-#             data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
-#             content_type="application/json",
-#         )
-#         self.assertEqual(response.status_code, 200)
-#         self.assertEqual(
-#             Event.objects.all()
-#             .filter(idempotency_id=self.valid_payload["idempotency_id"])
-#             .count(),
-#             1,
-#         )
+    def test_idempotency_duplicate_does_not_create_multiple_events(self):
+        """
+        Test that track_event does not create multiple events in the database if the idempotency_id is duplicated.
+        """
+        #first, ensure that the event_id we are sending is not cuirrently present
+        self.assertEqual(
+            Event.objects.all()
+                        .filter(idempotency_id=self.valid_payload["idempotency_id"])
+                        .count(),
+            0
+        )
+        #then send event, and expect success
+        response = self.client.post(
+            reverse("track_event"),
+            data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            Event.objects.all()
+            .filter(idempotency_id=self.valid_payload["idempotency_id"])
+            .count(),
+            1,
+        )
 
-#         #send the same event again, ennsure we get a bad request + no insertion
-#         response = self.client.post(
-#             reverse("track_event"),
-#             data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
-#             content_type="application/json",
-#         )
-#         self.assertEqual(response.status_code, 409)
-#         self.assertEqual(
-#             Event.objects.all()
-#             .filter(idempotency_id=self.valid_payload["idempotency_id"])
-#             .count(),
-#             1,
-#         )
+        #send the same event again, ennsure we get a bad request + no insertion
+        response = self.client.post(
+            reverse("track_event"),
+            data=json.dumps(self.valid_payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 409)
+        self.assertEqual(
+            Event.objects.all()
+            .filter(idempotency_id=self.valid_payload["idempotency_id"])
+            .count(),
+            1,
+        )
