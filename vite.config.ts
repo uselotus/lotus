3cae3bcd37a04274b33e551dde5e591f97238561
mnida <mikaelnida@gmail.com>
2022-08-17 22:52:38 -0700

    tweak vite build path

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   build: {
     manifest: true,
-    outDir: "./static/dist",
+    outDir: "../static/dist",
     emptyOutDir: true,
     rollupOptions: {
       input: {
