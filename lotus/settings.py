bd1a25825fc33de212c9325819fb4274dcb0594d
Diego Escobedo <diego@escobedo.com>
2022-08-18 22:17:58 -0500

    ssl changed again

--- lotus/settings.py
+++ lotus/settings.py
@@ -51,7 +51,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -129,7 +129,7 @@ try:
             os.environ["DATABASE_URL"],
             engine="django.db.backends.postgresql",
             conn_max_age=600,
-            ssl_require=ssl_require
+            ssl_require=ssl_require,
         )
     }
     django_heroku.settings(locals(), databases=False)
