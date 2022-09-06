5a42f0570543345e9771ee36ea3c5076bb5f9a54
mnida <mikaelnida@gmail.com>
2022-09-05 22:35:13 -0700

    linting

--- metering_billing/migrations/0003_alter_billingplan_components.py
+++ metering_billing/migrations/0003_alter_billingplan_components.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('metering_billing', '0002_alter_billingplan_flat_rate_and_more'),
+        ("metering_billing", "0002_alter_billingplan_flat_rate_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='billingplan',
-            name='components',
-            field=models.ManyToManyField(blank=True, to='metering_billing.plancomponent'),
+            model_name="billingplan",
+            name="components",
+            field=models.ManyToManyField(
+                blank=True, to="metering_billing.plancomponent"
+            ),
         ),
     ]
