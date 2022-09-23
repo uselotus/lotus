fb155b0caa21ed1ff49e59fee8e1e76c7f5ea4c3
Diego Escobedo <diego@uselotus.io>
2022-09-23 03:08:27 -0700

    added new registration endpoint (#148)

---
+++ metering_billing/tests/test_register.py
@@ -0,0 +1,170 @@
+import json
+
+import pytest
+from django.core.serializers.json import DjangoJSONEncoder
+from django.urls import reverse
+from metering_billing.models import Organization, User
+from model_bakery import baker
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+@pytest.fixture
+def registration_test_common_setup(
+    generate_org_and_api_key,
+    add_users_to_org,
+):
+    def do_registration_test_common_setup():
+        # set up organizations and api keys
+        org, key = generate_org_and_api_key()
+        org2, key2 = generate_org_and_api_key()
+        setup_dict = {
+            "org": org,
+            "key": key,
+            "org2": org2,
+            "key2": key2,
+        }
+        client = APIClient()
+        setup_dict["client"] = client
+        (user,) = add_users_to_org(org, n=1)
+        setup_dict["user"] = user
+
+        return setup_dict
+
+    return do_registration_test_common_setup
+
+
+@pytest.fixture
+def register_payload():
+    def generate_register_payload(company_name, industry, email, password, username):
+        payload = {
+            "register": {
+                "company_name": company_name,
+                "industry": industry,
+                "email": email,
+                "password": password,
+                "username": username,
+            }
+        }
+        return payload
+
+    return generate_register_payload
+
+
+@pytest.mark.django_db(transaction=True)
+class TestRegister:
+    """Testing the POST of Customer endpoint:
+    POST: Return list of customers associated with the organization with API key / user.
+    partitions:
+        auth_method: api_key, session_auth, both
+        num_customers_before_insert: 0, >0
+        user_org_and_api_key_org_different: true, false
+        customer_id
+    """
+
+    def test_register_valid(self, registration_test_common_setup, register_payload):
+        # covers num_customers_before_insert = 0, has_org_api_key=true, user_in_org=true, user_org_and_api_key_org_different=false
+        setup_dict = registration_test_common_setup()
+        users_before = User.objects.all().count()
+        organizations_before = Organization.objects.all().count()
+
+        payload = register_payload(
+            company_name="test",
+            industry="test",
+            email="test",
+            password="test",
+            username="test",
+        )
+
+        response = setup_dict["client"].post(
+            reverse("register"),
+            data=json.dumps(payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+
+        users_after = User.objects.all().count()
+        organizations_after = Organization.objects.all().count()
+        assert response.status_code == status.HTTP_201_CREATED
+        assert users_before + 1 == users_after
+        assert organizations_before + 1 == organizations_after
+
+    def test_register_org_exists(
+        self, registration_test_common_setup, register_payload
+    ):
+        setup_dict = registration_test_common_setup()
+        users_before = User.objects.all().count()
+        organizations_before = Organization.objects.all().count()
+
+        payload = register_payload(
+            company_name=setup_dict["org"].company_name,
+            industry="test",
+            email="test",
+            password="test",
+            username="test",
+        )
+
+        response = setup_dict["client"].post(
+            reverse("register"),
+            data=json.dumps(payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+
+        users_after = User.objects.all().count()
+        organizations_after = Organization.objects.all().count()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert users_before == users_after
+        assert organizations_before == organizations_after
+
+    def test_register_username_exists(
+        self, registration_test_common_setup, register_payload
+    ):
+        setup_dict = registration_test_common_setup()
+        users_before = User.objects.all().count()
+        organizations_before = Organization.objects.all().count()
+
+        payload = register_payload(
+            company_name="test",
+            industry="test",
+            email="test",
+            password="test",
+            username=setup_dict["user"].username,
+        )
+
+        response = setup_dict["client"].post(
+            reverse("register"),
+            data=json.dumps(payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+
+        users_after = User.objects.all().count()
+        organizations_after = Organization.objects.all().count()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert users_before == users_after
+        assert organizations_before == organizations_after
+
+    def test_register_email_exists(
+        self, registration_test_common_setup, register_payload
+    ):
+        setup_dict = registration_test_common_setup()
+        users_before = User.objects.all().count()
+        organizations_before = Organization.objects.all().count()
+
+        payload = register_payload(
+            company_name="test",
+            industry="test",
+            email=setup_dict["user"].email,
+            password="test",
+            username="test",
+        )
+
+        response = setup_dict["client"].post(
+            reverse("register"),
+            data=json.dumps(payload, cls=DjangoJSONEncoder),
+            content_type="application/json",
+        )
+
+        users_after = User.objects.all().count()
+        organizations_after = Organization.objects.all().count()
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert users_before == users_after
+        assert organizations_before == organizations_after
--- metering_billing/views/auth_views.py
+++ metering_billing/views/auth_views.py
@@ -4,7 +4,12 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.decorators.http import require_POST
-import uuid
+from drf_spectacular.utils import extend_schema
+from metering_billing.models import Organization, User
+from metering_billing.serializers.internal_serializers import RegistrationSerializer
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
 
 @require_POST
@@ -13,23 +18,30 @@ def login_view(request):
     try:
         data = json.loads(request.body)
     except json.JSONDecodeError:
-        return JsonResponse({"detail": "Invalid JSON."}, status=400)
+        return JsonResponse(
+            {"detail": "Invalid JSON."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     if data is None:
-        return JsonResponse({"detail": "No data provided."}, status=400)
+        return JsonResponse(
+            {"detail": "No data provided."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     username = data.get("username")
     password = data.get("password")
 
     if username is None or password is None:
         return JsonResponse(
-            {"detail": "Please provide username and password."}, status=400
+            {"detail": "Please provide username and password."},
+            status=status.HTTP_400_BAD_REQUEST,
         )
 
     user = authenticate(username=username, password=password)
 
     if user is None:
-        return JsonResponse({"detail": "Invalid credentials."}, status=400)
+        return JsonResponse(
+            {"detail": "Invalid credentials."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     login(request, user)
     # posthog.capture('test-id', 'test-event')
@@ -39,7 +51,9 @@ def login_view(request):
 @require_POST
 def logout_view(request):
     if not request.user.is_authenticated:
-        return JsonResponse({"detail": "You're not logged in."}, status=400)
+        return JsonResponse(
+            {"detail": "You're not logged in."}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     logout(request)
     return JsonResponse({"detail": "Successfully logged out."})
@@ -61,3 +75,42 @@ def whoami_view(request):
         return JsonResponse({"isAuthenticated": False})
 
     return JsonResponse({"username": request.user.username})
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    @extend_schema(
+        parameters=[RegistrationSerializer],
+    )
+    def post(self, request, format=None):
+        serializer = RegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        reg_dict = serializer.validated_data["register"]
+        existing_org_num = Organization.objects.filter(
+            company_name=reg_dict["company_name"]
+        ).count()
+        if existing_org_num > 0:
+            return JsonResponse(
+                {"detail": "Organization already exists."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        existing_user_num = User.objects.filter(username=reg_dict["username"]).count()
+        if existing_user_num > 0:
+            msg = f"User with username already exists"
+            return JsonResponse({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
+        existing_user_num = User.objects.filter(email=reg_dict["email"]).count()
+        if existing_user_num > 0:
+            msg = f"User with email already exists"
+            return JsonResponse({"detail": msg}, status=status.HTTP_400_BAD_REQUEST)
+        Organization.objects.create(
+            company_name=reg_dict["company_name"],
+        )
+        User.objects.create_user(
+            email=reg_dict["email"],
+            username=reg_dict["username"],
+            password=reg_dict["password"],
+        )
+        return JsonResponse(
+            {"detail": "Successfully registered."}, status=status.HTTP_201_CREATED
+        )
