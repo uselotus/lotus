8ce419905f208b6087395ab02e86bea1b3b7e916
Hardik <hardik@uselotus.io>
2022-10-29 18:08:58 +0530

    Wip

--- frontend/src/components/base/PageLayout.tsx
+++ frontend/src/components/base/PageLayout.tsx
@@ -1,16 +1,18 @@
-import { Layout, PageHeader, PageHeaderProps } from "antd";
+import {Layout, PageHeader, PageHeaderProps} from "antd";
+// @ts-ignore
 import React from "react";
 
-export const PageLayout = ({ children, ...props }: PageHeaderProps) => {
-  return (
-    <div>
-      <PageHeader
-        {...props}
-        title={<h1 className="text-3xl font-main ml-8 pr-1">{props.title} </h1>}
-      />
-      <Layout.Content className="m-6 min-h-[calc(100vh-210px)]">
-        {children}
-      </Layout.Content>
-    </div>
-  );
+export const PageLayout = ({children, ...props}: PageHeaderProps) => {
+    return (
+        <div>
+            <PageHeader title={<h1 className="text-2xl">Welcome back, Test</h1>}/>
+            <div className="flex items-center justify-between m-6">
+                <div className='text-3xl font-bold'>{props.title}</div>
+                <div>{props.extra}</div>
+            </div>
+            <Layout.Content className="m-6 min-h-[calc(100vh-210px)]">
+                {children}
+            </Layout.Content>
+        </div>
+    );
 };
