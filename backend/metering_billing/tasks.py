21111254856798122f8039800f5f377da73d9b4e
Diego Escobedo <diego@escobedo.com>
2022-11-04 02:17:35 -0700

    Fix backtest according to new plan versiosn setup. Closes LOT-142

--- backend/metering_billing/tasks.py
+++ backend/metering_billing/tasks.py
@@ -23,6 +23,8 @@ from metering_billing.serializers.backtest_serializers import (
     AllSubstitutionResultsSerializer,
 )
 from metering_billing.utils import (
+    date_as_max_dt,
+    date_as_min_dt,
     dates_bwn_two_dts,
     make_all_dates_times_strings,
     make_all_datetimes_dates,
@@ -196,14 +198,16 @@ def run_backtest(backtest_id):
     backtest_substitutions = backtest.backtest_substitutions.all()
     queries = [Q(billing_plan=x.original_plan) for x in backtest_substitutions]
     query = queries.pop()
+    start_date = date_as_min_dt(backtest.start_date)
+    end_date = date_as_max_dt(backtest.end_date)
     for item in queries:
         query |= item
     all_subs_time_period = (
         Subscription.objects.filter(
             query,
-            start_date__lte=backtest.end_date,
-            end_date__gte=backtest.start_date,
-            end_date__lte=backtest.end_date,
+            start_date__lte=end_date,
+            end_date__gte=start_date,
+            end_date__lte=end_date,
             organization=backtest.organization,
         )
         .prefetch_related("billing_plan")
@@ -318,6 +322,10 @@ def run_backtest(backtest_id):
         cum_rev_dict_list = []
         cum_rev = inner_results.pop("cumulative_revenue")
         cum_rev_lst = sorted(cum_rev.items(), key=lambda x: x[0], reverse=True)
+        date_cumrev_list = []
+        for date, cum_rev_dict in cum_rev_lst:
+            date_cumrev_list.append((date.date(),cum_rev_dict))
+        cum_rev_lst = date_cumrev_list
         every_date = list(dates_bwn_two_dts(cum_rev_lst[-1][0], cum_rev_lst[0][0]))
         date, rev_dict = cum_rev_lst.pop(-1)
         last_dict = {**rev_dict, "date": date}
