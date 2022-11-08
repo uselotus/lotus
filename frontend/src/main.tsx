aaf68fa219ff651e654a2dedc7e5b727a194396d
mnida <mikaelnida@gmail.com>
2022-11-08 14:29:16 -0800

    add cloud telemetry analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,19 @@ import "vite/modulepreload-polyfill";
 import "./index.css";
 import "./custom.less";
 import { QueryClient, QueryClientProvider } from "react-query";
+import posthog from "posthog-js";
+
+// Telemetry for Cloud Instances Only
+//Only Track Client Autocapture in Cloud Production Instances
+if (
+  !window.location.href.includes("127.0.0.1") &&
+  !window.location.href.includes("localhost") &&
+  import.meta.env.VITE_API_URL == "https://api.uselotus.io"
+) {
+  posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+    api_host: "https://app.posthog.com",
+  });
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
