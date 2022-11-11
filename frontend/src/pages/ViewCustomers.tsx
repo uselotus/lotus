bd5841a0f4a2cf2198138548ff056451c85e4881
mnida <mikaelnida@gmail.com>
2022-11-10 23:08:43 -0800

    import error to be fixed

--- frontend/src/pages/ViewCustomers.tsx
+++ frontend/src/pages/ViewCustomers.tsx
@@ -6,8 +6,7 @@ import {
   CustomerType,
 } from "../types/customer-type";
 import { Customer } from "../api/api";
-
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 import LoadingSpinner from "../components/LoadingSpinner";
 import {
   useQuery,
@@ -20,10 +19,13 @@ import CreateCustomerForm, {
   CreateCustomerState,
 } from "../components/Customers/CreateCustomerForm";
 import { toast } from "react-toastify";
+import { Importer, ImporterField } from "react-csv-importer";
+import "react-csv-importer/dist/index.css";
 
 const ViewCustomers: FC = () => {
   const queryClient = useQueryClient();
   const [visible, setVisible] = useState(false);
+  const [visibleImport, setVisibleImport] = useState(false);
 
   const { data, isLoading }: UseQueryResult<CustomerPlus[]> = useQuery<
     CustomerPlus[]
@@ -54,6 +56,21 @@ const ViewCustomers: FC = () => {
     }
   );
 
+  //batch create customers
+  const importCustomers = useMutation(
+    (post: CustomerType) => Customer.createCustomer(post),
+    {
+      onSuccess: () => {
+        setVisible(false);
+        queryClient.invalidateQueries(["customer_list"]);
+        queryClient.invalidateQueries(["customer_totals"]);
+        toast.success("Customer created successfully", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      },
+    }
+  );
+
   const openCustomerModal = () => {
     setVisible(true);
   };
@@ -61,6 +78,12 @@ const ViewCustomers: FC = () => {
   const onCancel = () => {
     setVisible(false);
   };
+
+  const onCancelImport = () => {
+    setVisibleImport(false);
+  };
+
+  const onImport = () => {};
 
   const onSave = (state: CreateCustomerState) => {
     const customerInstance: CustomerType = {
@@ -86,6 +109,54 @@ const ViewCustomers: FC = () => {
         ) : (
           <CustomerTable customerArray={data} totals={customerTotals} />
         )}
+        <Modal
+          title="Import Customers"
+          visible={visibleImport}
+          onCancel={onCancelImport}
+        >
+          <Importer
+            assumeNoHeaders={false} // optional, keeps "data has headers" checkbox off by default
+            restartable={false} // optional, lets user choose to upload another file when import is complete
+            onStart={({ file, preview, fields, columnFields }) => {
+              // optional, invoked when user has mapped columns and started import
+              // prepMyAppForIncomingData();
+            }}
+            processChunk={async (rows, { startIndex }) => {
+              // required, may be called several times
+              // receives a list of parsed objects based on defined fields and user column mapping;
+              // (if this callback returns a promise, the widget will wait for it before parsing more data)
+              for (var row of rows) {
+                console.log(row);
+              }
+            }}
+            onComplete={({ file, preview, fields, columnFields }) => {
+              // optional, invoked right after import is done (but user did not dismiss/reset the widget yet)
+              toast.success("Import complete", {
+                position: toast.POSITION.TOP_CENTER,
+              });
+            }}
+            onClose={({ file, preview, fields, columnFields }) => {
+              // optional, if this is specified the user will see a "Finish" button after import is done,
+              // which will call this when clicked
+              onCancelImport();
+            }}
+
+            // CSV options passed directly to PapaParse if specified:
+            // delimiter={...}
+            // newline={...}
+            // quoteChar={...}
+            // escapeChar={...}
+            // comments={...}
+            // skipEmptyLines={...}
+            // delimitersToGuess={...}
+            // chunkSize={...} // defaults to 10000
+            // encoding={...} // defaults to utf-8, see FileReader API
+          >
+            <ImporterField name="customer_id" label="customer_id" />
+            <ImporterField name="email" label="email" />
+            <ImporterField name="stripe_id" label="stripe_id" optional />
+          </Importer>
+        </Modal>
         <CreateCustomerForm
           visible={visible}
           onSave={onSave}
