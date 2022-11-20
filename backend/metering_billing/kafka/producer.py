f35323795b1d2f0c826abf2ad8a9171ba13df2b6
Diego Escobedo <diego@escobedo.com>
2022-11-19 18:56:03 -0800

    quick wip

--- backend/metering_billing/kafka/producer.py
+++ backend/metering_billing/kafka/producer.py
@@ -5,7 +5,6 @@ from django.conf import settings
 
 from .singleton import Singleton
 
-POSTHOG_PERSON = settings.POSTHOG_PERSON
 KAFKA_EVENTS_TOPIC = settings.KAFKA_EVENTS_TOPIC
 PRODUCER = settings.PRODUCER
 
@@ -19,6 +18,7 @@ class Producer(metaclass=Singleton):
 
     def produce(self, customer_id, stream_events):
         print(f"Producing record. key={customer_id}, value={stream_events}")
+        print(KAFKA_EVENTS_TOPIC, self.__connection)
         self.__connection.send(
             topic=KAFKA_EVENTS_TOPIC,
             key=customer_id.encode("utf-8"),
