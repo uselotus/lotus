96a718996eae5fe56adbcde1db40e942d2d968aa
Diego Escobedo <diego@escobedo.com>
2022-11-20 14:10:49 -0800

    push

--- backend/metering_billing/kafka/producer.py
+++ backend/metering_billing/kafka/producer.py
@@ -24,5 +24,6 @@ class Producer(metaclass=Singleton):
             key=customer_id.encode("utf-8"),
             value=json.dumps(stream_events).encode("utf-8"),
         )
+        print(KAFKA_EVENTS_TOPIC, self.__connection.__dict__)
         self.__connection.flush()
         print("finished sending message")
