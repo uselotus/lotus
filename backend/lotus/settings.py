b1496798a50d173ee329e048f9cf690d8906ee55
Diego Escobedo <diego@escobedo.com>
2022-11-21 11:19:21 -0800

    hm

--- backend/lotus/settings.py
+++ backend/lotus/settings.py
@@ -323,6 +323,7 @@ if KAFKA_HOST:
     PRODUCER = KafkaProducer(**producer_config)
     CONSUMER = KafkaConsumer(KAFKA_EVENTS_TOPIC, **consumer_config)
     print(PRODUCER.__dict__["_sender"].__dict__)
+    print("PRODUCER PRODUCER")
     ADMIN_CLIENT = KafkaAdminClient(**admin_client_config)
 
     existing_topics = ADMIN_CLIENT.list_topics()
