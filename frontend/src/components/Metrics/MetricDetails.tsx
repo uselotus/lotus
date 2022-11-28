5a9c22466e9a2ccb816c9319a307b378b08e4aac
mnida <mikaelnida@gmail.com>
2022-11-28 00:46:13 -0800

    update

--- frontend/src/components/Metrics/MetricDetails.tsx
+++ frontend/src/components/Metrics/MetricDetails.tsx
@@ -1,79 +1,106 @@
-import {Modal, Tag} from "antd";
-import {MetricType} from "../../types/metric-type";
+import { Modal, Tag } from "antd";
+import { MetricType } from "../../types/metric-type";
 // @ts-ignore
-import React, {FC, Fragment} from "react";
-import {colorMap} from "./MetricTable";
+import React, { FC, Fragment } from "react";
+import { colorMap } from "./MetricTable";
 
 interface MetricDetailsProps {
-    metric: MetricType;
-    onclose: () => void;
+  metric: MetricType;
+  onclose: () => void;
 }
 
 const metricTypeColorMap = new Map<string, string>([
-    ["counter", "green"],
-    ["stateful", "blue"],
-    ["rate", "purple"],
+  ["counter", "green"],
+  ["stateful", "blue"],
+  ["rate", "purple"],
 ]);
 
 const eventTypeColorMap = new Map<string, string>([
-    ["delta", "green"],
-    ["total", "blue"],
+  ["delta", "green"],
+  ["total", "blue"],
 ]);
 
+const MetricDetails: FC<MetricDetailsProps> = ({ metric, onclose }) => {
+  return (
+    <Modal
+      visible
+      title={
+        <b>
+          {" "}
+          {metric?.billable_metric_name
+            ? metric.billable_metric_name
+            : "Metric"}{" "}
+        </b>
+      }
+      cancelText="Close"
+      width={800}
+      onCancel={onclose}
+      onOk={onclose}
+    >
+      <div className="py-4 grid grid-cols-2 items-start justify-between  px-8 rounded-lg bg-[#FFFFFF]  border-2 border-solid  border-[#EAEAEB]">
+        <p>
+          <b className="mr-2">Event Name:</b> {metric.event_name}
+        </p>
+        <p>
+          <b className="mr-2">Cost Metric:</b>{" "}
+          {metric.is_cost_metric ? "Yes" : "No"}
+        </p>
+        <p>
+          <b className="mr-2">Property Name:</b> {metric.property_name ?? "N/A"}
+        </p>
+        <p>
+          <b className="mr-2">Metric Type:</b>
+          <Tag>{metric.metric_type}</Tag>
+        </p>
+        <p>
+          <b className="mr-2">Per Time Unit:</b>{" "}
+          {metric.granularity === "total" ? "none" : metric.granularity}
+        </p>
 
-const MetricDetails: FC<MetricDetailsProps> = ({metric, onclose}) => {
-    return (
-        <Modal
-            visible
-            title={<b> {metric?.billable_metric_name ? metric.billable_metric_name : "Metric"} Details</b>}
-            cancelText="Close"
-            width={800}
-            onCancel={onclose}
-            onOk={onclose}
-        >
-            <div
-                className="py-4 flex items-start justify-between  px-8 rounded-lg bg-[#FFFFFF]  border-2 border-solid rounded border-[#EAEAEB]">
-                <div>
-                    <p><b className="mr-2">Id:</b> {metric.id ? metric.id : "N/A"}</p>
-                    <p><b className="mr-2">Event Name:</b> {metric.event_name}</p>
-                    <p><b className="mr-2">Property Name:</b> {metric.property_name}</p>
-                    <p><b className="mr-2">Billable Metric
-                        Name:</b> {metric.billable_metric_name ? metric.billable_metric_name : "N/A"}</p>
-                    <p><b className="mr-2">Granularity:</b> {metric.granularity ? metric.granularity : "N/A"}</p>
-                </div>
-                <div>
-                    <p><b className="mr-2">Usage Aggregation Type:</b>
-                        <Tag color={colorMap.get(metric.usage_aggregation_type)}>
-                            {metric.usage_aggregation_type}
-                        </Tag>
-                    </p>
-                    <p><b className="mr-2">Billable Aggregation Type:</b>
-                        {!!metric.billable_aggregation_type ?
-                            (
-                                <Tag color="yellow">
-                                    {metric.billable_aggregation_type}
-                                </Tag>
-                            ) : "N/A"
-                        }
-                    </p>
-                    <p><b className="mr-2">Metric Type:</b>
-                        <Tag color={metricTypeColorMap.get(metric.metric_type)}>
-                            {metric.metric_type}
-                        </Tag>
-                    </p>
-                    <p><b className="mr-2">Event Type:</b>
-                        {!!metric.event_type ?
-                            (
-                                <Tag color={eventTypeColorMap.get(metric.event_type)}>
-                                    {metric.event_type}
-                                </Tag>
-                            ) : "N/A"
-                        }
-                    </p>
-                </div>
-            </div>
-        </Modal>
-    );
+        <p>
+          <b className="mr-2">Usage Aggregation Type:</b>
+          <Tag color={colorMap.get(metric.usage_aggregation_type)}>
+            {metric.usage_aggregation_type}
+          </Tag>
+        </p>
+        <p>
+          <b className="mr-2">Billable Aggregation Type:</b>
+          {!!metric.billable_aggregation_type ? (
+            <Tag>{metric.billable_aggregation_type}</Tag>
+          ) : (
+            "N/A"
+          )}
+        </p>
+
+        <p>
+          <b className="mr-2">Event Type:</b>
+          {!!metric.event_type ? <Tag>{metric.event_type}</Tag> : "N/A"}
+        </p>
+        <p>
+          <b className="mr-2">Filters:</b>
+        </p>
+        <div></div>
+        <div>
+          {metric.numeric_filters?.map((filter, index) => (
+            <Tag color="" key={filter.property_name}>
+              {<b>{filter.property_name}</b>} {filter.operator} {'"'}
+              {filter.comparison_value}
+              {'"'}
+            </Tag>
+          ))}
+        </div>
+        <div>
+          {metric.categorical_filters?.map((filter, index) => (
+            <Tag color="" key={filter.property_name}>
+              {<b>{filter.property_name}</b>} {filter.operator} {'"'}
+              {filter.comparison_value}
+              {'"'}
+            </Tag>
+          ))}
+        </div>
+      </div>
+    </Modal>
+  );
 };
 
 export default MetricDetails;
