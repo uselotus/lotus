f1757ce456d53bdd0999c39d27781c78d62ad4d3
mnida <mikaelnida@gmail.com>
2022-12-05 00:36:14 -0800

    update types

--- frontend/src/components/Customers/CustomerDetail.tsx
+++ frontend/src/components/Customers/CustomerDetail.tsx
@@ -1,7 +1,7 @@
 // @ts-ignore
 import React, { useEffect, useState } from "react";
 import { Form, Tabs, Modal } from "antd";
-import { PlanType } from "../../types/plan-type";
+import { PlanType} from "../../types/plan-type";
 import {
   CreateSubscriptionType,
   TurnSubscriptionAutoRenewOffType,
@@ -20,6 +20,7 @@ import {
 import {
   CustomerDetailType,
   CustomerDetailSubscription,
+  DetailPlan,
 } from "../../types/customer-type";
 import "./CustomerDetail.css";
 import CustomerInvoiceView from "./CustomerInvoices";
@@ -45,7 +46,7 @@ function CustomerDetail(props: {
   const [endDate, setEndDate] = useState<string>(dayjs().format("YYYY-MM-DD"));
 
   const [customerSubscriptions, setCustomerSubscriptions] = useState<
-    CustomerDetailSubscription[]
+    DetailPlan[]
   >([]);
 
   const { data, isLoading }: UseQueryResult<CustomerDetailType> =
@@ -53,7 +54,7 @@ function CustomerDetail(props: {
       ["customer_detail", props.customer_id],
       () =>
         Customer.getCustomerDetail(props.customer_id).then((res) => {
-          setCustomerSubscriptions(res.subscriptions);
+          setCustomerSubscriptions(res.subscription.plans);
           return res;
         }),
       {
