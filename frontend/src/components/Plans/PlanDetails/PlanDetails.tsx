80a8cf7bf70f4777f9a14d9a83253444e8d406ca
Diego Escobedo <diego@escobedo.com>
2022-12-12 11:47:26 -0800

    minor fixes

--- frontend/src/components/Plans/PlanDetails/PlanDetails.tsx
+++ frontend/src/components/Plans/PlanDetails/PlanDetails.tsx
@@ -144,8 +144,11 @@ const PlanDetails: FC = () => {
           ></PageLayout>
           <div className="mx-10">
             <div className="planDetails">
-              <div className="pr-1 planDetailsLabel">Plan Id:</div>
-              <div className="planDetailsValue"> <CopyText textToCopy={plan.plan_id}/></div>
+              <div className="pr-1 planDetailsLabel">Plan ID:</div>
+              <div className="planDetailsValue">
+                {" "}
+                <CopyText textToCopy={plan.plan_id} />
+              </div>
             </div>
             <div className="planDetails">
               <div className="pr-1 planDetailsLabel">Plan Duration:</div>
