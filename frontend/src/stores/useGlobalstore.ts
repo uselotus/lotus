ef51209216249dea7f2c6178cbc25bd519b173ea
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-12-20 11:55:42 +0100

    feat: initial global store implementation

---
+++ frontend/src/stores/useGlobalstore.ts
@@ -0,0 +1,22 @@
+import create from "zustand";
+type GlobalStoreState = {
+  username: string;
+  org: IOrg;
+};
+interface IOrg {
+  company_name: string;
+  currency: string;
+  environment?: string;
+}
+
+const useGlobalStore = create<GlobalStoreState>((set) => ({
+  username: "",
+  org: {
+    company_name: "",
+    currency: "",
+    environment: undefined,
+  },
+  setUsername: (username: string) => set({ username }),
+  setOrgInfo: (org: IOrg) => set({ org }),
+}));
+export default useGlobalStore;
