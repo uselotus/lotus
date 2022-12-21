15d1e0028cc2d7f172f7b51385485f6c6b29659a
Diego Escobedo <diego@escobedo.com>
2022-12-20 22:25:32 -0800

    push

--- backend/metering_billing/middleware.py
+++ backend/metering_billing/middleware.py
@@ -15,41 +15,45 @@ class OrganizationInsertMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        st = f"OrganizationInsertMiddleware BEFORE: {request.user}, {request.user.is_authenticated}"
-        if request.user.is_authenticated:
-            try:
-                st += f", {request.user.organization}"
-            except:
-                pass
-        logger.info()
-        if request.user.is_authenticated:
-            organization = request.user.organization
-        else:
-            api_key_checker = HasUserAPIKey()
-            api_key = api_key_checker.get_key(request)
-            if api_key is None:
-                organization = None
+        try:
+            st = f"OrganizationInsertMiddleware BEFORE: {request.user}, {request.user.is_authenticated}"
+            if request.user.is_authenticated:
+                try:
+                    st += f", {request.user.organization}"
+                except:
+                    pass
+            logger.info(st)
+            if request.user.is_authenticated:
+                organization = request.user.organization
             else:
-                prefix, _, _ = api_key.partition(".")
-                organization_pk = cache.get(prefix)
-                if not organization_pk:
-                    try:
-                        api_token = APIToken.objects.get_from_key(api_key)
-                        organization = api_token.organization
-                        organization_pk = api_token.organization.pk
-                        expiry_date = api_token.expiry_date
-                        timeout = (
-                            60 * 60 * 24
-                            if expiry_date is None
-                            else (expiry_date - now_utc()).total_seconds()
-                        )
-                        cache.set(prefix, organization_pk, timeout)
-                    except:
-                        organization = None
+                api_key_checker = HasUserAPIKey()
+                api_key = api_key_checker.get_key(request)
+                if api_key is None:
+                    organization = None
                 else:
-                    organization = Organization.objects.get(pk=organization_pk)
-        logger.info(f"OrganizationInsertMiddleware: {organization}, {request.user}")
-        request.organization = organization
+                    prefix, _, _ = api_key.partition(".")
+                    organization_pk = cache.get(prefix)
+                    if not organization_pk:
+                        try:
+                            api_token = APIToken.objects.get_from_key(api_key)
+                            organization = api_token.organization
+                            organization_pk = api_token.organization.pk
+                            expiry_date = api_token.expiry_date
+                            timeout = (
+                                60 * 60 * 24
+                                if expiry_date is None
+                                else (expiry_date - now_utc()).total_seconds()
+                            )
+                            cache.set(prefix, organization_pk, timeout)
+                        except:
+                            organization = None
+                    else:
+                        organization = Organization.objects.get(pk=organization_pk)
+            logger.info(f"OrganizationInsertMiddleware: {organization}, {request.user}")
+            request.organization = organization
+        except Exception as e:
+            logger.error(f"OrganizationInsertMiddleware: {e}")
+            pass
         # Code to be executed for each request before
         # the view (and later middleware) are called.
         response = self.get_response(request)
