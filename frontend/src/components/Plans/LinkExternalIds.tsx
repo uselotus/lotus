3de055e0801032465cd1bbbd388bad27877e24d9
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-12-21 19:38:02 +0100

    Resolve "Creating & deleting external plans – Optimistic UI." (#400)

--- frontend/src/components/Plans/LinkExternalIds.tsx
+++ frontend/src/components/Plans/LinkExternalIds.tsx
@@ -2,7 +2,7 @@ import { PlusOutlined } from "@ant-design/icons";
 import type { InputRef } from "antd";
 import { Input, Tag, Tooltip } from "antd";
 // @ts-ignore
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, memo } from "react";
 
 interface LinkExternalIdsProps {
   externalIds: string[];
@@ -11,13 +11,11 @@ interface LinkExternalIdsProps {
   deleteExternalLink?: (link) => void;
 }
 
-const LinkExternalIds: React.FC<LinkExternalIdsProps> = ({
-  externalIds,
-  setExternalLinks,
+const LinksExternalIds: React.FC<LinkExternalIdsProps> = ({
+  externalIds: tags,
   createExternalLink,
   deleteExternalLink,
 }) => {
-  const [tags, setTags] = useState<string[]>(externalIds);
   const [inputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const inputRef = useRef<InputRef>(null);
@@ -34,9 +32,6 @@ const LinkExternalIds: React.FC<LinkExternalIdsProps> = ({
   }, [inputValue]);
 
   const handleClose = (removedTag: string) => {
-    const newTags = tags.filter((tag) => tag !== removedTag);
-    setTags(newTags);
-    setExternalLinks && setExternalLinks(newTags);
     deleteExternalLink && deleteExternalLink(removedTag);
   };
 
@@ -46,9 +41,6 @@ const LinkExternalIds: React.FC<LinkExternalIdsProps> = ({
 
   const handleInputConfirm = () => {
     if (inputValue && tags.indexOf(inputValue) === -1) {
-      const links = [...tags, inputValue];
-      setTags(links);
-      setExternalLinks && setExternalLinks(links);
       createExternalLink && createExternalLink(inputValue);
     }
     setInputVisible(false);
@@ -97,5 +89,5 @@ const LinkExternalIds: React.FC<LinkExternalIdsProps> = ({
     </>
   );
 };
-
+const LinkExternalIds = memo(LinksExternalIds);
 export default LinkExternalIds;
