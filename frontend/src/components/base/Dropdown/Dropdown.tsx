ac38404909b6ab7e1d76cd7662c6729111c065bf
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-12-22 20:17:23 +0100

    feat: open drop down on hover

--- frontend/src/components/base/Dropdown/Dropdown.tsx
+++ frontend/src/components/base/Dropdown/Dropdown.tsx
@@ -2,33 +2,68 @@ import React, { PropsWithChildren } from "react";
 interface DropdownProps {
   className?: string;
 }
-const Dropdown = ({ children }: PropsWithChildren<DropdownProps>) => (
-  <div>{children}</div>
-);
+interface DropdownContextState {
+  isOpen: boolean;
+  openHandler: VoidFunction;
+  closeHandler: VoidFunction;
+}
+const DropdownContext = React.createContext({} as DropdownContextState);
 
+const Dropdown = ({ children }: PropsWithChildren<DropdownProps>) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const openHandler = React.useCallback(() => setIsOpen(true), []);
+  const closeHandler = React.useCallback(() => setIsOpen(false), []);
+  const value = React.useMemo(
+    () => ({ isOpen, openHandler, closeHandler }),
+    [isOpen]
+  );
+  return (
+    <div>
+      <DropdownContext.Provider value={value}>
+        {children}
+      </DropdownContext.Provider>
+    </div>
+  );
+};
+const useDropdownContext = () => {
+  const context = React.useContext(DropdownContext);
+  if (!context) {
+    throw new Error(
+      `Toggle compound components cannot be rendered outside the Toggle component`
+    );
+  }
+  return context;
+};
 const Container: React.FC<PropsWithChildren<DropdownProps>> = ({
   children,
   className,
-}) => (
-  <div
-    className={
-      !className
-        ? "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-        : [
-            "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
-            className,
-          ].join(" ")
-    }
-    role="menu"
-    aria-orientation="vertical"
-    aria-labelledby="menu-button"
-    tabIndex={-1}
-  >
-    <div className="py-1" role="none">
-      {children}
-    </div>
-  </div>
-);
+}) => {
+  const { isOpen } = useDropdownContext();
+  return (
+    <>
+      {isOpen && (
+        <div
+          className={
+            !className
+              ? "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              : [
+                  "absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
+                  className,
+                ].join(" ")
+          }
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="menu-button"
+          tabIndex={-1}
+        >
+          <div className="py-1" role="none">
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 
 const MenuItem: React.FC<PropsWithChildren<DropdownProps>> = ({
   children,
@@ -50,7 +85,14 @@ const MenuItem: React.FC<PropsWithChildren<DropdownProps>> = ({
 
 const DropdownTrigger: React.FC<PropsWithChildren<DropdownProps>> = ({
   children,
-}) => <div>{children}</div>;
+}) => {
+  const { openHandler, closeHandler } = useDropdownContext();
+  return (
+    <div onMouseEnter={openHandler} onMouseLeave={closeHandler}>
+      {children}
+    </div>
+  );
+};
 
 Dropdown.Container = Container;
 Dropdown.MenuItem = MenuItem;
