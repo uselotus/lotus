d2fa46b9e7668cdad6c6e6846c69c558e7b95e8e
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2022-12-28 00:52:45 +0100

    feat: create scripts for dev and self-hosting

---
+++ scripts/self-host.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# check to see what plarform 
+if [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]];
+then
+    # debian or Windows 
+   FILE=env/.env.prod
+if [ -f "$FILE" ]; then
+    echo "Reading prod environment variables 🚀"
+    echo "Building and running Docker image! 🚀"
+    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+else 
+echo "Creating prod environment variables 🚀"
+ copy env/.env.prod.example env/.env.prod
+ echo "env file created."
+ echo "Please consider replacing the .env.prod file content with custom values!"
+ echo "Building and running Docker image! 🚀"
+ docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+ fi
+else  
+  # macOS OSX or Linux
+  FILE=env/.env.prod
+if [ -f "$FILE" ]; then
+    echo "Reading prod environment variables 🚀"
+    echo "Building and running Docker image! 🚀"
+    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+else 
+echo "Creating prod environment variables 🚀"
+ cp env/.env.prod.example env/.env.prod
+ echo "env file created."
+ echo "Please consider replacing the .env.prod file content with custom values!"
+ echo "Building and running Docker image! 🚀"
+ docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+fi
+fi
