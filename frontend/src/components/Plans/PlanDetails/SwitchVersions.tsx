e242cf7e252eef98a064e7a96c9f8c8dd567b1d0
Diego Escobedo <diego@escobedo.com>
2023-01-10 16:15:46 -0800

    merge alerts

--- frontend/src/components/Plans/PlanDetails/SwitchVersions.tsx
+++ frontend/src/components/Plans/PlanDetails/SwitchVersions.tsx
@@ -183,17 +183,11 @@ const SwitchVersions: FC<SwitchVersionProps> = ({
             updateBillingFrequencyMutation={updateBillingFrequency.mutate}
             plan={plan}
             components={selectedVersion.components}
-          />
-        </div>
-
-        <div className="px-4 py-2">
-          <PlanComponents
-            components={selectedVersion.components}
             alerts={selectedVersion.alerts}
             plan_version_id={selectedVersion.version_id}
           />
         </div>
-        <div className="px-4 py-2">
+        <div>
           <PlanFeatures features={selectedVersion.features} />
         </div>
 
