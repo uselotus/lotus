adba93de1cd764115d2f586daf098f7367457b29
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-01-13 12:42:28 +0100

    feat: silence docker and check for images

--- scripts/self-host.sh
+++ scripts/self-host.sh
@@ -6,29 +6,62 @@ then
    FILE=env/.env.prod
 if [ -f "$FILE" ]; then
     echo "Reading prod environment variables 🚀"
-    echo "Building and running Docker image! 🚀"
-    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+
+  echo "Building and running Docker image! 🚀"
+    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build --quiet-pull
+
+else 
+echo "Running Docker image! 🚀"
+  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up 
+fi
+
 else 
 echo "Creating prod environment variables 🚀"
  copy env/.env.prod.example env/.env.prod
  echo "env file created."
  echo "Please consider replacing the .env.prod file content with custom values!"
- echo "Building and running Docker image! 🚀"
- docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+  if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+
+  echo "Building and running Docker image! 🚀"
+  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build --quiet-pull
+
+else 
+echo "Running Docker image! 🚀"
+  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up 
+
+ fi
  fi
 else  
   # macOS OSX or Linux
   FILE=env/.env.prod
 if [ -f "$FILE" ]; then
     echo "Reading prod environment variables 🚀"
-    echo "Building and running Docker image! 🚀"
-    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+    echo "I do not get na"
+  echo "Building and running Docker image! 🚀"
+    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build --quiet-pull
+
+else 
+echo "Running Docker image! 🚀"
+  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --quiet-pull
+fi
+
 else 
 echo "Creating prod environment variables 🚀"
  cp env/.env.prod.example env/.env.prod
  echo "env file created."
  echo "Please consider replacing the .env.prod file content with custom values!"
- echo "Building and running Docker image! 🚀"
- docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+
+    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+
+  echo "Building and running Docker image! 🚀"
+    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build --quiet-pull
+
+else 
+echo "Running Docker image! 🚀"
+  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up 
+fi
+
 fi
 fi
