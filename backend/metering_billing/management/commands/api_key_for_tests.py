356953dc355008bb2abe9a093ca02368c988abe5
Diego Escobedo <diego@escobedo.com>
2023-02-09 11:56:11 -0600

    generate api key for tests

---
+++ backend/metering_billing/management/commands/api_key_for_tests.py
@@ -0,0 +1,23 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+
+from metering_billing.models import Organization
+from metering_billing.serializers.model_serializers import APITokenSerializer
+
+
+class Command(BaseCommand):
+    "Django command to execute calculate invoice"
+
+    def handle(self, *args, **options):
+        management.call_command("initadmin")
+
+        org = Organization.objects.all().first()
+
+        _, key = APITokenSerializer().create(
+            {
+                "name": "test",
+                "organization": org,
+            }
+        )
+
+        print(key)
