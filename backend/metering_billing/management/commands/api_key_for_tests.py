347d58f080256b64e3c2d34f75ed1ee16f13ef00
Diego Escobedo <diego@escobedo.com>
2023-02-09 12:28:25 -0600

    changes

--- backend/metering_billing/management/commands/api_key_for_tests.py
+++ backend/metering_billing/management/commands/api_key_for_tests.py
@@ -1,6 +1,4 @@
-from django.core import management
 from django.core.management.base import BaseCommand
-
 from metering_billing.models import Organization
 from metering_billing.serializers.model_serializers import APITokenSerializer
 
@@ -9,8 +7,6 @@ class Command(BaseCommand):
     "Django command to execute calculate invoice"
 
     def handle(self, *args, **options):
-        management.call_command("initadmin")
-
         org = Organization.objects.all().first()
 
         _, key = APITokenSerializer().create(
---
+++ backend/metering_billing/management/commands/plan_for_tests.py
@@ -0,0 +1,22 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+from metering_billing.demos import setup_demo3
+from metering_billing.models import Organization, Plan
+
+
+class Command(BaseCommand):
+    "Django command to execute calculate invoice"
+
+    def handle(self, *args, **options):
+        management.call_command("initadmin")
+
+        setup_demo3(
+            organization_name="demo3",
+            username="demo3",
+            email="demo3@demo3.com",
+            password="demo3",
+            org_type=Organization.OrganizationType.INTERNAL_DEMO,
+        )
+
+        plan = Plan.objects.all().first()
+        print(plan.plan_id)
