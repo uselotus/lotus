30eb4166a095e58d187a3fceb25e1600e7f79cbb
Diego Escobedo <diego@escobedo.com>
2023-02-09 15:21:14 -0600

    no celery

--- backend/metering_billing/management/commands/postman_ci_outputs.py
+++ backend/metering_billing/management/commands/postman_ci_outputs.py
@@ -1,6 +1,7 @@
 import uuid
 
 from django.core.management.base import BaseCommand
+
 from metering_billing.aggregation.billable_metrics import METRIC_HANDLER_MAP
 from metering_billing.demos import create_pc_and_tiers, make_subscription_record
 from metering_billing.invoice import generate_invoice
@@ -157,7 +158,7 @@ class Command(BaseCommand):
         )
 
         # invoice
-        invoice = generate_invoice(sr)[0]
+        invoice = generate_invoice(sr, draft=True)[0]
         print(f"INVOICE_ID={invoice.invoice_id.hex}")
 
         # credit
