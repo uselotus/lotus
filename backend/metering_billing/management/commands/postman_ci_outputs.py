8be017123ac2a19e75403fd738705426aeb86d5b
Diego Escobedo <diego@escobedo.com>
2023-02-09 23:17:44 -0600

    log

--- backend/metering_billing/management/commands/postman_ci_outputs.py
+++ backend/metering_billing/management/commands/postman_ci_outputs.py
@@ -2,6 +2,7 @@ import logging
 import uuid
 
 from django.core.management.base import BaseCommand
+
 from metering_billing.aggregation.billable_metrics import METRIC_HANDLER_MAP
 from metering_billing.demos import create_pc_and_tiers, make_subscription_record
 from metering_billing.invoice import generate_invoice
@@ -213,6 +214,8 @@ class Command(BaseCommand):
         print(f"FEATURE_ID=feature_{premium_support_feature.feature_id.hex}")
         print(f"EVENT_NAME={sum_words.event_name}")
         print(f"FEATURE_NAME={premium_support_feature.feature_name}")
+        logger.info(f"UNUSED_METRIC_ID=metric_{unused_metric.metric_id.hex}")
+        logger.info(f"METRIC_ID=metric_{sum_words.metric_id.hex}")
 
         # custometr
         print(f"CUSTOMER_ID={customer.customer_id}")
