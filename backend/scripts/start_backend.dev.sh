f647f98643f8da2c8e0c40ecb79db78b87f6b57b
Jonathan MERCIER <bioinfornatics@gmail.com>
2023-02-12 10:04:45 +0100

    update accordingly scripts

--- backend/scripts/start_backend.dev.sh
+++ backend/scripts/start_backend.dev.sh
@@ -1,9 +1,9 @@
 while ! nc -q 1 db 5432 </dev/null; do sleep 5; done
 while ! nc -q 1 svix-server 8071 </dev/null; do sleep 5; done
 
-python3 manage.py wait_for_db && \
-python3 manage.py migrate && \
-python3 manage.py demo_up && \
-python3 manage.py initadmin && \
-python3 manage.py setup_tasks && \
-python3 manage.py runserver 0.0.0.0:8000
+python3 lotus_backend wait_for_db && \
+python3 lotus_backend migrate && \
+python3 lotus_backend demo_up && \
+python3 lotus_backend initadmin && \
+python3 lotus_backend setup_tasks && \
+python3 lotus_backend runserver 0.0.0.0:8000
--- backend/scripts/start_consumer.sh
+++ backend/scripts/start_consumer.sh
@@ -1,3 +1,3 @@
 while ! nc -q 1 db 5432 </dev/null; do sleep 5; done
 
-python3 manage.py event_consumer
+python3 lotus_backend event_consumer
