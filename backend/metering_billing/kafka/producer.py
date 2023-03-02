d05b77dba4dea4816521c5d0d42a6fe0165888b9
Diego Escobedo <diego@uselotus.io>
2023-03-02 15:27:31 -0800

    send invoices to kafka topic (#629)

--- backend/metering_billing/kafka/producer.py
+++ backend/metering_billing/kafka/producer.py
@@ -1,15 +1,29 @@
 import json
 import logging
+from datetime import date, datetime
+from decimal import Decimal
 
 from django.conf import settings
 from kafka import KafkaProducer
+from metering_billing.models import Invoice
 
 from .singleton import Singleton
 
 KAFKA_EVENTS_TOPIC = settings.KAFKA_EVENTS_TOPIC
+KAFKA_INVOICE_TOPIC = settings.KAFKA_INVOICE_TOPIC
 producer_config = settings.PRODUCER_CONFIG
 
 logger = logging.getLogger("django.server")
+
+
+class InvoiceEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        elif isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        else:
+            return super().default(obj)
 
 
 class Producer(metaclass=Singleton):
@@ -26,3 +40,24 @@ class Producer(metaclass=Singleton):
             value=json.dumps(stream_events).encode("utf-8"),
         )
         logger.info(f"Produced record to topic {KAFKA_EVENTS_TOPIC}")
+
+    def produce_invoice(self, invoice: Invoice):
+        from api.serializers.model_serializers import InvoiceSerializer
+
+        assert isinstance(invoice, Invoice), "invoice must be an instance of Invoice"
+        invoice_data = InvoiceSerializer(invoice).data
+        message = {
+            "messageType": "update",
+            "team": invoice.organization.organization_id.hex,
+            "payload": invoice_data,
+        }
+        logger.info(f"Producing invoice. key={invoice.invoice_id.hex}, value={invoice}")
+        self.__connection.send(
+            topic=KAFKA_INVOICE_TOPIC,
+            key=invoice.invoice_id.hex.encode("utf-8"),
+            value=json.dumps(message, cls=InvoiceEncoder).encode("utf-8"),
+        )
+        logger.info(f"Produced invoice to topic {KAFKA_INVOICE_TOPIC}")
+
+    def test(self):
+        logger.info("test")
