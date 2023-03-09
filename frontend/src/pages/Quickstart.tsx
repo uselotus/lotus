fb1066e951f9a3a58d7abad0b045c2737d762a6e
mnida <mikaelnida@gmail.com>
2023-03-08 22:55:52 -0800

    everything except text formatting

--- frontend/src/pages/Quickstart.tsx
+++ frontend/src/pages/Quickstart.tsx
@@ -10,7 +10,8 @@ import {
   Webhook,
   PaymentProcessorIntegration,
 } from "../api/api";
-
+import useGlobalStore from "../stores/useGlobalstore";
+import CopyText from "../components/base/CopytoClipboard";
 interface Props {
   text: string;
   subText: string;
@@ -19,6 +20,7 @@ interface Props {
   time: string;
   userAction?: string;
   link?: string;
+  highlighted?: boolean;
 }
 
 interface QuickStartCheckProps {
@@ -29,6 +31,79 @@ interface QuickStartCheckProps {
   setHasPaymentConnected?: (value: boolean) => void;
   setUsersInOrg?: (value: boolean) => void;
 }
+
+const CodeExample = () => {
+  const [selectedTab, setSelectedTab] = useState("curl");
+
+  const codeExamples = {
+    curl: {
+      label: "Curl",
+      code: `curl --request POST \
+      --url https://api.uselotus.io/api/track/ \
+      --header 'X-API-KEY: AUTH_VALUE' \
+      --header 'Content-Type: application/json' \
+      --data '{
+  "batch": "array"
+ }'`,
+    },
+    pythonSDK: {
+      label: "Python SDK",
+      code: `lotus.track_event(
+        customer_id='customer123',
+        event_name='api_call',
+        properties={
+            'region': 'US',
+            'mb_used': 150
+          },
+        idempotency_id='c9799bf9-e5c9-4007-8d10-0663d045d23c',
+        time_created="2023-01-01T21:58:14.193Z"
+      )`,
+    },
+    typescriptSDK: {
+      label: "TypeScript SDK",
+      code: `await lotus.trackEvent({
+        batch: [
+          {
+            event_name: "test", // required
+            time_created: new Date(), // optional, if not provided current time will be taken
+            customer_id: "cust_5894767364aa4e64", // required
+            properties: { test: "test", numeric_quantity: 3.1415 }, //optional, pass in any additional properties you want to aggregate or measure
+            idempotency_id: "c2c5eb5d-de4b-44e0", //optional if not provided Randomly generated ID will be taken
+          },
+        ],
+      });`,
+    },
+  };
+
+  const handleTabClick = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className="p-4 pt-0 bg-[#F5F5F5] w-full">
+      <div className="flex space-x-4 mb-12">
+        {Object.keys(codeExamples).map((key) => (
+          <button
+            key={key}
+            onClick={() => handleTabClick(key)}
+            className={`px-4 py-2 text-white text-sm rounded-md ${
+              selectedTab === key
+                ? " bg-darkgold"
+                : "bg-gray-500 hover:bg-gray-600"
+            }`}
+          >
+            {codeExamples[key].label}
+          </button>
+        ))}
+      </div>
+      <CopyText
+        textToCopy={codeExamples[selectedTab].code}
+        className=" text-sm mx-8"
+        showIcon
+      />
+    </div>
+  );
+};
 
 const quickStartCheck = async ({
   setHasAPIKey,
@@ -120,39 +195,35 @@ const quickStartItem = ({
   complete,
   icon,
   time,
-  userAction,
   link,
+  highlighted,
 }: Props): JSX.Element => {
   if (link) {
     return (
       <a
         target={`${link.includes("https") ? "_blank" : "_self"}`}
         rel="noopener noreferrer"
-        className={`w-full ${
-          complete && "opacity-30 hover:opacity-100 duration-200"
-        }`}
+        className={`w-full ${complete && " hover:opacity-100 duration-150"}`}
         href={link}
       >
         <div
           onKeyDown={() => null}
           role="button"
           tabIndex={0}
-          className="relative group bg-gray-300 hover:bg-gold  duration-200 rounded-md border pl-2 pr-6 py-2 h-[5.5rem] w-full flex items-center justify-between overflow-hidden mb-3 cursor-pointer"
+          className={`relative group text-black  duration-200 rounded-md border pl-2 pr-6 py-2 h-[5.5rem] w-full flex items-center justify-between overflow-hidden mb-3 cursor-pointer ${
+            complete ? "bg-[#E3FFF1]" : "bg-[#F5F5F5] "
+          }
+            ${highlighted && " border-2 border-b-8 border-[#BF9F79] "}
+          `}
         >
-          <div className="flex flex-row items-center mr-4">
-            <i className={"text-4xl mx-2 w-16" + icon} />
-            {complete && (
-              <div className="bg-bunker-500 group-hover:bg-gold w-7 h-7 rounded-full absolute left-12 top-10 p-2 flex items-center justify-center">
-                <i className="ri-checkbox-circle-fill"></i>
-              </div>
-            )}
+          <div className="flex flex-row items-center mr-4 ml-4">
             <div className="flex flex-col items-start">
               <div className="text-xl font-semibold mt-0.5">{text}</div>
               <div className="text-sm font-normal">{subText}</div>
             </div>
           </div>
           <div
-            className={`pr-4 font-semibold text-sm w-28 text-right ${
+            className={`pr-4 font-semibold text-sm text-right ${
               complete && "text-primary"
             }`}
           >
@@ -166,36 +237,37 @@ const quickStartItem = ({
     );
   }
   return (
-    <div
-      onKeyDown={() => null}
-      role="button"
-      tabIndex={0}
-      className="relative bg-bunker-700 hover:bg-bunker-500 shadow-xl duration-200 rounded-md border border-dashed border-bunker-400 pl-2 pr-6 py-2 h-[5.5rem] w-full flex items-center justify-between overflow-hidden my-1.5 cursor-pointer"
-    >
-      <div className="flex flex-row items-center mr-4">
-        <i className={"text-4xl mx-2 w-16" + icon} />
-
-        {complete && (
-          <div className="bg-bunker-700 w-7 h-7 rounded-full absolute left-11 top-10">
-            <i className="ri-checkbox-circle-fill"></i>
-          </div>
-        )}
-        <div className="flex flex-col items-start">
-          <div className="text-xl font-semibold mt-0.5">{text}</div>
-          <div className="text-sm font-normal mt-0.5">{subText}</div>
-        </div>
-      </div>
+    <>
       <div
-        className={`pr-4 font-semibold text-sm w-28 text-right ${
-          complete && "text-primary"
-        }`}
+        onKeyDown={() => null}
+        role="button"
+        tabIndex={0}
+        className={`relative group text-black duration-150 rounded-md border pl-2 pr-6 py-2 h-[5.5rem] w-full flex items-center justify-between overflow-hidden mb-3 ${
+          complete ? "bg-[#E3FFF1]" : "bg-[#F5F5F5] "
+        }
+        ${highlighted && " border-2 border-b-8 border-[#BF9F79] "}
+
+      `}
       >
-        {complete ? "Complete!" : `About ${time}`}
+        <div className="flex flex-row items-center mr-4 ml-4">
+          <div className="flex flex-col items-start">
+            <div className="text-xl font-semibold mt-0.5">{text}</div>
+            <div className="text-sm font-normal mt-0.5">{subText}</div>
+          </div>
+        </div>
+        <div
+          className={`pr-4 font-semibold text-sm text-right ${
+            complete && "text-primary"
+          }`}
+        >
+          {complete ? "Complete!" : `About ${time}`}
+        </div>
+        {complete && (
+          <div className="absolute bottom-0 left-0 h-1 w-full bg-primary" />
+        )}
       </div>
-      {complete && (
-        <div className="absolute bottom-0 left-0 h-1 w-full bg-primary" />
-      )}
-    </div>
+      <CodeExample />
+    </>
   );
 };
 
@@ -206,6 +278,10 @@ const QuickstartPage: FC = () => {
   const [hasCreatedPlan, setHasCreatedPlan] = useState(false);
   const [hasPaymentConnected, setHasPaymentConnected] = useState(false);
   const [usersInOrg, setUsersInOrg] = useState(false);
+  const [currentItem, setCurrentItem] = useState(0);
+  const { current_user, environment, organization_name } = useGlobalStore(
+    (state) => state.org
+  );
 
   useEffect(() => {
     quickStartCheck({
@@ -217,108 +293,104 @@ const QuickstartPage: FC = () => {
       setUsersInOrg,
     });
   }, []);
+
+  useEffect(() => {
+    if (hasAPIKey && !hasTrackedEvent) {
+      setCurrentItem(1);
+    }
+    if (hasAPIKey && hasTrackedEvent && !hasCreatedMetric) {
+      setCurrentItem(2);
+    }
+    if (hasAPIKey && hasTrackedEvent && hasCreatedMetric && !hasCreatedPlan) {
+      setCurrentItem(3);
+    }
+    if (
+      hasAPIKey &&
+      hasTrackedEvent &&
+      hasCreatedMetric &&
+      hasCreatedPlan &&
+      !hasPaymentConnected
+    ) {
+      setCurrentItem(4);
+    }
+    if (
+      hasAPIKey &&
+      hasTrackedEvent &&
+      hasCreatedMetric &&
+      hasCreatedPlan &&
+      hasPaymentConnected
+    ) {
+      setCurrentItem(5);
+    }
+  }, [
+    hasAPIKey,
+    hasTrackedEvent,
+    hasCreatedMetric,
+    hasCreatedPlan,
+    hasPaymentConnected,
+  ]);
+
   return (
     <PageLayout title="Get Started Fast" extra={[]}>
       <div>
-        <div className="flex relative flex-col items-center text-lg mx-auto px-6 max-w-3xl lg:max-w-4xl xl:max-w-5xl py-6">
+        <div className="flex relative flex-col items-center text-lg mx-auto space-y-6 px-6 max-w-3xl lg:max-w-4xl xl:max-w-5xl py-6">
           <div className="text-4xl font-bold text-left w-full mt-12">
-            Your quick start guide
+            {organization_name}'s quick start guide
           </div>
-          <div className="text-lg text-left w-full pt-2 pb-4 mb-14 text">
+          <div className="text-lg text-left w-full pt-2 pb-4 mb-14">
             Click on the items below and follow the instructions.
           </div>
 
           {quickStartItem({
-            text: "Create an API Key",
+            text: "1. Create an API Key",
             subText: "",
             complete: hasAPIKey,
             icon: "faHandPeace",
             time: "30 sec",
             userAction: "intro_cta_clicked",
             link: `/settings/developer-settings`,
+            highlighted: currentItem === 0,
           })}
-          {/* {learningItem({
-            text: "Add your secrets",
-            subText: "Click to see example secrets, and add your own.",
-            complete: hasUserPushedSecrets,
-            icon: faPlus,
-            time: "2 min",
-            userAction: "first_time_secrets_pushed",
-            link: `/dashboard/${router.query.id}`,
-          })}
-          <div className="relative group bg-bunker-500 shadow-xl duration-200 rounded-md border border-mineshaft-600 pl-2 pr-2 pt-4 pb-2 h-full w-full flex flex-col items-center justify-between overflow-hidden mb-3 cursor-default">
-            <div className="w-full flex flex-row items-center mb-4 pr-4">
-              <div className="flex flex-row items-center mr-4 w-full">
-                <FontAwesomeIcon
-                  icon={faNetworkWired}
-                  className="text-4xl mx-2 w-16"
-                />
-                {false && (
-                  <div className="bg-bunker-500 group-hover:bg-mineshaft-700 w-7 h-7 rounded-full absolute left-12 top-10 p-2 flex items-center justify-center">
-                    <FontAwesomeIcon
-                      icon={faCheckCircle}
-                      className="text-4xl w-5 h-5 text-green"
-                    />
-                  </div>
-                )}
-                <div className="flex pl-0.5 flex-col items-start">
-                  <div className="text-xl font-semibold mt-0.5">
-                    Inject secrets locally
-                  </div>
-                  <div className="text-sm font-normal">
-                    Replace .env files with a more secure and efficient
-                    alternative.
-                  </div>
-                </div>
-              </div>
-              <div
-                className={`pr-4 font-semibold text-sm w-28 text-right ${
-                  false && "text-green"
-                }`}
-              >
-                About 2 min
-              </div>
-            </div>
-            <TabsObject />
-            {false && (
-              <div className="absolute bottom-0 left-0 h-1 w-full bg-green" />
-            )}
-          </div> */}
 
           {quickStartItem({
-            text: "Track your first event",
+            text: "2. Track your first event",
             subText: "",
             complete: hasTrackedEvent,
             icon: "ri-user-add-line",
             time: "3 min",
+            highlighted: currentItem === 1,
           })}
+
           {quickStartItem({
-            text: "Create a Metric",
-            subText: "Use a template provide for the quickest start.",
+            text: "3. Create a Metric",
+            subText: "",
             complete: hasCreatedMetric,
             icon: "ri-user-add-line",
             time: "1 min",
             link: `/metrics`,
+            highlighted: currentItem === 2,
           })}
           {quickStartItem({
-            text: "Create a Plan",
+            text: "4. Create a Plan",
             subText: "",
             complete: hasCreatedPlan,
             icon: "ri-user-add-line",
             time: "2 min",
-            link: `/plans/create-plan`,
+            link: `/create-plan`,
+            highlighted: currentItem === 3,
           })}
           {quickStartItem({
-            text: "Connect a payment method or a webhook endpoint",
+            text: "5. Connect a payment method or a webhook endpoint",
             subText:
-              "Ensure that you have a way to collect payment for invoices that are generated in Lotus, either through Stripe, Braintree, or your own integration built ontop of our webhooks.",
+              "Ensure that you have a way to collect payment for invoices.",
             complete: hasPaymentConnected,
             icon: "ri-user-add-line",
             time: "2 min",
             link: `/settings/integrations`,
+            highlighted: currentItem === 4,
           })}
           {quickStartItem({
-            text: "Join Lotus Slack",
+            text: "6. Join Lotus Slack",
             subText:
               "Ask any specific or general questions in our community Slack!",
             complete: false,
@@ -328,7 +400,7 @@ const QuickstartPage: FC = () => {
             link: "https://join.slack.com/t/lotus-community/shared_invite/zt-1fufuktbp-ignnw768aZgdFNlcvAOSrw",
           })}
           {quickStartItem({
-            text: "Star Lotus on GitHub",
+            text: "7. Star Lotus on GitHub",
             subText: "Want to show some open-source love? Give us a star! :)",
             complete: false,
             icon: "ri-star-line",
