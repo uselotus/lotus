546b931504b8e11e4250b4821f36c87fd6b28da1
UnderKoen <koenvanstaveren@hotmail.com>
2023-03-15 13:06:23 +0100

    fix: correctly format bash scripts

--- scripts/self-host.sh
+++ scripts/self-host.sh
@@ -1,71 +1,70 @@
 #!/bin/bash
 # check to see what plarform
-if [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]];
-then
-    # debian or Windows
-   FILE=env/.env.prod
-if [ -f "$FILE" ]; then
+if [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+  # debian or Windows
+  FILE=env/.env.prod
+  if [ -f "$FILE" ]; then
     echo "Reading prod environment variables 🚀"
 
-    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+    if [[ "$(docker image inspect lotus-frontend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2>/dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2>/dev/null)" == "" ]]; then
 
-  echo "Building and running Docker image! 🚀"
-    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+      echo "Building and running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
 
-else 
-echo "Running Docker image! 🚀"
-  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
-fi
+    else
+      echo "Running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    fi
 
-else 
+  else
 
-echo "Creating prod environment variables 🚀"
- copy env/.env.prod.example env/.env.prod
- echo "env file created."
- echo "Please consider replacing the .env.prod file content with custom values!"
-  if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+    echo "Creating prod environment variables 🚀"
+    copy env/.env.prod.example env/.env.prod
+    echo "env file created."
+    echo "Please consider replacing the .env.prod file content with custom values!"
+    if [[ "$(docker image inspect lotus-frontend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2>/dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2>/dev/null)" == "" ]]; then
 
-  echo "Building and running Docker image! 🚀"
-  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build 
+      echo "Building and running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
 
-else 
-echo "Running Docker image! 🚀"
-  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    else
+      echo "Running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
 
- fi
- fi
+    fi
+  fi
 else
   # macOS OSX or Linux
   FILE=env/.env.prod
-if [ -f "$FILE" ]; then
+  if [ -f "$FILE" ]; then
     echo "Reading prod environment variables 🚀"
 
-    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect docker.redpanda.com/vectorized/redpanda:v22.2.2 2> /dev/null)" == "" ]]; then
-    echo "I do not get na"
-  echo "Building and running Docker image! 🚀"
-    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    if [[ "$(docker image inspect lotus-frontend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2>/dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect docker.redpanda.com/vectorized/redpanda:v22.2.2 2>/dev/null)" == "" ]]; then
+      echo "I do not get na"
+      echo "Building and running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
 
-else 
-echo "Running Docker image! 🚀"
-  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
-fi
+    else
+      echo "Running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    fi
 
-else 
+  else
 
-echo "Creating prod environment variables 🚀"
- cp env/.env.prod.example env/.env.prod
- echo "env file created."
- echo "Please consider replacing the .env.prod file content with custom values!"
+    echo "Creating prod environment variables 🚀"
+    cp env/.env.prod.example env/.env.prod
+    echo "env file created."
+    echo "Please consider replacing the .env.prod file content with custom values!"
 
-    if [[ "$(docker image inspect lotus-frontend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2> /dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2> /dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2> /dev/null)" == "" ]]; then
+    if [[ "$(docker image inspect lotus-frontend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-event_consumer:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-backend:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect lotus-celery-beat:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect svix/svix-server:latest 2>/dev/null)" == "" ]] && [[ "$(docker image inspect redis:7-alpine 2>/dev/null)" == "" ]] && [[ "$(docker image inspect timescale/timescaledb-ha:latest 2>/dev/null)" == "" ]]; then
 
-  echo "Building and running Docker image! 🚀"
-    docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build 
+      echo "Building and running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
 
-else 
-echo "Running Docker image! 🚀"
-  docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
-fi
+    else
+      echo "Running Docker image! 🚀"
+      docker-compose -f docker-compose.prod.yaml --env-file env/.env.prod up --build
+    fi
 
-fi
+  fi
 fi
