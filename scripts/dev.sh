c86f8eb7f129fde05298aaae5f7496c1b0c2af1d
Ben Reilly <benreilly0809@gmail.com>
2023-03-27 10:17:15 -0700

    dev script starts up in linux now

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -15,6 +15,21 @@ if [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "win64" ]]; then
     echo "Building and running Docker image! 🚀"
     docker-compose -f docker-compose.dev.yaml --env-file env/.env.dev up --build
   fi
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # macOS OSX or Linux
+  FILE=env/.env.dev
+  if [ -f "$FILE" ]; then
+    echo "Reading dev environment variables 🚀"
+    echo "Building and running Docker image! 🚀"
+    docker compose -f docker-compose.dev.yaml --env-file env/.env.dev up --build
+  else
+    echo "Creating dev environment variables 🚀"
+    cp env/.env.dev.example env/.env.dev
+    echo "env file created."
+    echo "Please consider replacing the .env.dev file content with custom values!"
+    echo "Building and running Docker image! 🚀"
+    docker compose -f docker-compose.dev.yaml --env-file env/.env.dev up --build
+  fi
 else
   # macOS OSX or Linux
   FILE=env/.env.dev
